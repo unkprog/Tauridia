0ed07808b0c0a1eca3b0ebaa00568670ccdca825
Bezhnar Sergey <bsisoftx@mail.ru>
2020-08-28 18:20:42 +0300

    Добавление новых сущностей

---
+++ Tauridia.Core/Models/Record/Attribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tauridia.Core.Models.Record
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class BaseAttribute : Attribute
+    {
+        // Имя
+        public string Name { get; set; }
+        // Описание
+        public string Description { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RecordClassAttribute : BaseAttribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RecordPropertyAttribute : BaseAttribute
+    {
+    }
+}
---
+++ Tauridia.Core/Models/Record/RecordModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tauridia.Core.Models.Record
+{
+    public abstract class RecordModel : ModelBase
+    {
+    }
+}
