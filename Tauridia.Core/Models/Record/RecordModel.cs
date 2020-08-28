352207f1e0250f6e420e413c7d24d97d2820b7ce
unkprog <unkprog@gmail.com>
2020-08-28 20:04:34 +0300

    Update RecordModel.cs

--- Tauridia.Core/Models/Record/RecordModel.cs
+++ Tauridia.Core/Models/Record/RecordModel.cs
@@ -7,4 +7,10 @@ namespace Tauridia.Core.Models.Record
     public abstract class RecordModel : ModelBase
     {
     }
+
+public abstract class Record<T> : RecordModel
+{
+    public T Id {get;set;}
+}
+
 }
