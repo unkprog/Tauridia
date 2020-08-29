d373f8608a8f3f509b035b6dc7f474f6a07966b7
unkprog <unkprog@gmail.com>
2020-08-29 16:54:55 +0300

    Update RecordModel.cs

--- Tauridia.Core/Models/Record/RecordModel.cs
+++ Tauridia.Core/Models/Record/RecordModel.cs
@@ -8,9 +8,25 @@ namespace Tauridia.Core.Models.Record
     {
     }
 
-public abstract class Record<T> : RecordModel
-{
-    public T Id {get;set;}
-}
+    public abstract class Record<T> : RecordModel
+    {
+        public T Id { get; set; }
+        public bool Deleted { get; set; }
+    }
 
+    public abstract class RecordHierarhy<T> : Record<T>
+    {
+        public T ParentId { get; set; }
+    }
+
+    public abstract class RecordReference<T> : RecordHierarhy<T>
+    {
+        public string Name { get; set; }
+    }
+
+    public abstract class RecordDocument<T> : RecordHierarhy<T>
+    {
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+    }
 }
