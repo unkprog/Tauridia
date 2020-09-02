60addb1dc5ac9461408fa8f6e043947d4e3d3bcd
Sergey Bezhnar <unkprog@gmail.com>
2020-09-03 00:04:12 +0300

    Добавлено view настроек

---
+++ Tauridia.App/Views/Settings.axaml.ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using ReactiveUI;
+
+namespace Tauridia.App.ViewModels.Settings
+{
+    public class ViewModel : ViewModelBase
+    {
+        internal static readonly string fileSettings = "Settings.config";
+        internal static readonly string pathSettings = string.Concat(Environment.CurrentDirectory, @"\Settings");
+
+        public ViewModel()
+        {
+            //CurrentContent = new ViewModels.WelcomeSreen.ViewModel();
+        }
+
+        private string _server = "https://localhost:44331";
+
+        [DataMember]
+        public string Server
+        {
+            get => _server;
+            set => this.RaiseAndSetIfChanged(ref _server, value);
+        }
+
+        public void Load()
+        {
+            CheckDirectorySettings();
+        }
+
+        public void Save()
+        {
+            CheckDirectorySettings();
+        }
+
+        private string CheckDirectorySettings()
+        {
+            string result = pathSettings;
+            if (!Directory.Exists(pathSettings))
+                Directory.CreateDirectory(pathSettings);
+            return result;
+        }
+    }
+}
