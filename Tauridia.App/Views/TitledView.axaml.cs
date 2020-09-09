c40cc26fe8d7211412177e988b2889858c9c603a
Sergey Bezhnar <unkprog@gmail.com>
2020-09-10 00:04:43 +0300

    Рефакторинг

---
+++ Tauridia.App/Views/TitledView.axaml.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Tauridia.App.Views
+{
+    public partial class TitledView : UserControl
+    {
+        public TitledView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            this.Content
+        }
+
+        public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<TitledView, string>(nameof(Title));
+        public string Title
+        {
+            get { return GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly StyledProperty<bool> VisibleCheckProperty = AvaloniaProperty.Register<TitledView, bool>(nameof(VisibleCheck));
+        public bool VisibleCheck
+        {
+            get { return GetValue(VisibleCheckProperty); }
+            set { SetValue(VisibleCheckProperty, value); }
+        }
+
+        public static readonly StyledProperty<bool> VisibleCloseProperty = AvaloniaProperty.Register<TitledView, bool>(nameof(VisibleClose));
+        public bool VisibleClose
+        {
+            get { return GetValue(VisibleCloseProperty); }
+            set { SetValue(VisibleCloseProperty, value); }
+        }
+
+
+    }
+}
---
+++ Tauridia.App/Views/WelcomeViewModel.Properties.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Tauridia.Core.Models.Connection;
+
+namespace Tauridia.App.Views
+{
+    public partial class WelcomeViewModel
+    {
+        [DataMember]
+        public ObservableCollection<ConnectionServer> ListServers => App.Settings.ConnectionsServers.ListServers;
+
+
+        private ConnectionServer _selectedConnectionServer;
+
+        [IgnoreDataMember]
+        public ConnectionServer SelectedConnectionServer
+        {
+            get => _selectedConnectionServer;
+            set => this.RaiseAndSetIfChanged(ref _selectedConnectionServer, value);
+        }
+    }
+}
