d94cb8635f6fb650c0d1cd7d8edc8a2b05066ca9
Bezhnar Sergey <unkprog@gmail.com>
2020-09-15 18:06:36 +0300

    Новое представление "Старт"

--- Tauridia.App/Views/StartViewModel.Properties.cs
+++ Tauridia.App/Views/StartViewModel.Properties.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ReactiveUI;
+using System.Reactive;
 
 namespace Tauridia.App.Views
 {
     public partial class StartViewModel
     {
+        public ReactiveCommand<Unit, Unit> OpenProjectCommand => ReactiveCommand.Create(() => {  });
+        public ReactiveCommand<Unit, Unit> CreateProjectCommand => ReactiveCommand.Create(() => { });
+        public ReactiveCommand<Unit, Unit> OpenInfobaseCommand => ReactiveCommand.Create(() => { });
+        public ReactiveCommand<Unit, Unit> CreateInfobaseCommand => ReactiveCommand.Create(() => { });
     }
 }
