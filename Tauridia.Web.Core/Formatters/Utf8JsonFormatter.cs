e8592b201f79b2dc997cb0bd7313c23264059ef7
Bezhnar Sergey <unkprog@gmail.com>
2020-09-18 18:38:18 +0300

    Рефакторинг

---
+++ Tauridia.Web.Core/Formatters/Utf8JsonFormatter.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using System.IO;
+using System.Threading.Tasks;
+using Utf8Json;
+
+namespace Tauridia.Web.Core.Formatters
+{
+    public class Utf8JsonOutputFormatter : IOutputFormatter //, IApiResponseTypeMetadataProvider
+    {
+        const string ContentType = "application/json";
+        static readonly string[] SupportedContentTypes = new[] { ContentType };
+
+        readonly IJsonFormatterResolver resolver;
+
+        public Utf8JsonOutputFormatter()
+            : this(null)
+        {
+
+        }
+        public Utf8JsonOutputFormatter(IJsonFormatterResolver resolver)
+        {
+            this.resolver = resolver ?? JsonSerializer.DefaultResolver;
+        }
+
+        //public IReadOnlyList<string> GetSupportedContentTypes(string contentType, Type objectType)
+        //{
+        //    return SupportedContentTypes;
+        //}
+
+        public bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            return true;
+        }
+
+        public Task WriteAsync(OutputFormatterWriteContext context)
+        {
+            context.HttpContext.Response.ContentType = ContentType;
+
+            // when 'object' use the concrete type(object.GetType())
+            if (context.ObjectType == typeof(object))
+            {
+                return JsonSerializer.NonGeneric.SerializeAsync(context.HttpContext.Response.Body, context.Object, resolver);
+            }
+            else
+            {
+                return JsonSerializer.NonGeneric.SerializeAsync(context.ObjectType, context.HttpContext.Response.Body, context.Object, resolver);
+            }
+        }
+    }
+
+    public class Utf8JsonInputFormatter : IInputFormatter // , IApiRequestFormatMetadataProvider
+    {
+        const string ContentType = "application/json";
+        static readonly string[] SupportedContentTypes = new[] { ContentType };
+
+        readonly IJsonFormatterResolver resolver;
+
+        public Utf8JsonInputFormatter()
+            : this(null)
+        {
+
+        }
+
+        public Utf8JsonInputFormatter(IJsonFormatterResolver resolver)
+        {
+            this.resolver = resolver ?? JsonSerializer.DefaultResolver;
+        }
+
+        //public IReadOnlyList<string> GetSupportedContentTypes(string contentType, Type objectType)
+        //{
+        //    return SupportedContentTypes;
+        //}
+
+        public bool CanRead(InputFormatterContext context)
+        {
+            return true;
+        }
+
+        public Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            var request = context.HttpContext.Request;
+
+            //byte[] ss = new byte[request.Body.Length + 10];
+            //request.Body.Read(ss, 0, (int)request.Body.Length);
+            //string sss = System.Text.UTF8Encoding.UTF8.GetString(ss);
+            var reader = new StreamReader(request.Body);
+            //reader.BaseStream.Seek(0, SeekOrigin.Begin);
+            var rawMessage = reader.ReadToEnd();
+            //var result0 = JsonSerializer.NonGeneric.Deserialize(typeof(Project), rawMessage);
+            var result = JsonSerializer.NonGeneric.Deserialize(context.ModelType, rawMessage, resolver);
+            //var result = JsonSerializer.NonGeneric.Deserialize(context.ModelType, new StreamReader(request.Body).BaseStream, resolver);
+            return InputFormatterResult.SuccessAsync(result);
+        }
+    }
+}
--- Tauridia.Web.Server/Startup.cs
+++ Tauridia.Web.Server/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.IO;
-using System.Text.Unicode;
-using System.Threading.Tasks;
-using Tauridia.Core.Models.Project;
-using Utf8Json;
-using Utf8Json.AspNetCoreMvcFormatter;
+using Tauridia.Web.Core.Formatters;
 using Utf8Json.Resolvers;
 
 namespace Tauridia.Web.Server
@@ -32,9 +26,9 @@ namespace Tauridia.Web.Server
             services.AddControllers().AddMvcOptions(option =>
             {
                 option.OutputFormatters.Clear();
-                option.OutputFormatters.Add(new JsonOutputFormatter123(StandardResolver.Default));
+                option.OutputFormatters.Add(new Utf8JsonOutputFormatter(StandardResolver.Default));
                 option.InputFormatters.Clear();
-                option.InputFormatters.Add(new JsonInputFormatter123());
+                option.InputFormatters.Add(new Utf8JsonInputFormatter());
             });
             services.Configure<KestrelServerOptions>(options =>
             {
@@ -69,91 +63,5 @@ namespace Tauridia.Web.Server
     }
 
 
-    public class JsonOutputFormatter123 : IOutputFormatter //, IApiResponseTypeMetadataProvider
-    {
-        const string ContentType = "application/json";
-        static readonly string[] SupportedContentTypes = new[] { ContentType };
-
-        readonly IJsonFormatterResolver resolver;
-
-        public JsonOutputFormatter123()
-            : this(null)
-        {
-
-        }
-        public JsonOutputFormatter123(IJsonFormatterResolver resolver)
-        {
-            this.resolver = resolver ?? JsonSerializer.DefaultResolver;
-        }
-
-        //public IReadOnlyList<string> GetSupportedContentTypes(string contentType, Type objectType)
-        //{
-        //    return SupportedContentTypes;
-        //}
-
-        public bool CanWriteResult(OutputFormatterCanWriteContext context)
-        {
-            return true;
-        }
-
-        public Task WriteAsync(OutputFormatterWriteContext context)
-        {
-            context.HttpContext.Response.ContentType = ContentType;
-
-            // when 'object' use the concrete type(object.GetType())
-            if (context.ObjectType == typeof(object))
-            {
-                return JsonSerializer.NonGeneric.SerializeAsync(context.HttpContext.Response.Body, context.Object, resolver);
-            }
-            else
-            {
-                return JsonSerializer.NonGeneric.SerializeAsync(context.ObjectType, context.HttpContext.Response.Body, context.Object, resolver);
-            }
-        }
-    }
-
-    public class JsonInputFormatter123 : IInputFormatter // , IApiRequestFormatMetadataProvider
-    {
-        const string ContentType = "application/json";
-        static readonly string[] SupportedContentTypes = new[] { ContentType };
-
-        readonly IJsonFormatterResolver resolver;
-
-        public JsonInputFormatter123()
-            : this(null)
-        {
-
-        }
-
-        public JsonInputFormatter123(IJsonFormatterResolver resolver)
-        {
-            this.resolver = resolver ?? JsonSerializer.DefaultResolver;
-        }
-
-        //public IReadOnlyList<string> GetSupportedContentTypes(string contentType, Type objectType)
-        //{
-        //    return SupportedContentTypes;
-        //}
-
-        public bool CanRead(InputFormatterContext context)
-        {
-            return true;
-        }
-
-        public Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
-        {
-            var request = context.HttpContext.Request;
-
-            //byte[] ss = new byte[request.Body.Length + 10];
-            //request.Body.Read(ss, 0, (int)request.Body.Length);
-            //string sss = System.Text.UTF8Encoding.UTF8.GetString(ss);
-            var reader = new StreamReader(request.Body);
-            //reader.BaseStream.Seek(0, SeekOrigin.Begin);
-            var rawMessage = reader.ReadToEnd();
-            //var result0 = JsonSerializer.NonGeneric.Deserialize(typeof(Project), rawMessage);
-            var result = JsonSerializer.NonGeneric.Deserialize(context.ModelType, rawMessage, resolver);
-            //var result = JsonSerializer.NonGeneric.Deserialize(context.ModelType, new StreamReader(request.Body).BaseStream, resolver);
-            return InputFormatterResult.SuccessAsync(result);
-        }
-    }
+ 
 }
